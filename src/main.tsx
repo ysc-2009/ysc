85829b7ea4847caf6a7a91fe02aaee0b883a3af3
github jjjoker0323 <bbyuue0323@docomo.ne.jp>
2026-03-08 19:37:29 +0900

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={__BASE_PATH__}>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
